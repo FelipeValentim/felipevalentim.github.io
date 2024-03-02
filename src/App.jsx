36f8b1879a5bffe3c3ceff3a2c9277192eca012b
Felipe Valentim <felipe@practicesoftware.com.br>
2024-03-02 19:02:55 -0300

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,36 @@ import Header from "./NavBar";
 import "./App.css";
 import About from "./About";
 import Home from "./Home";
+import { useLottie } from "lottie-react";
+import loading from "./loading.json";
+import React from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const Loading = () => {
+    const options = {
+      animationData: loading,
+      loop: false,
+      onComplete: () => setIsLoading(false),
+    };
+
+    const { View } = useLottie(options);
+
+    return <div className="loading">{View}</div>;
+  };
+
   return (
     <div className="main-container">
-      <Header />
-      <Home />
-      <About />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="fade">
+          <Header />
+          <Home />
+          <About />
+        </div>
+      )}
     </div>
   );
 }
