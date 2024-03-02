b01fb8c51a706b9bb6a8b23509c453c0f452a00a
Felipe Valentim <felipe@practicesoftware.com.br>
2024-03-02 12:59:41 -0300

    about

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import programming from "/programming.svg";
 import "./About.css";
 
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCode,
+  faDatabase,
+  faDownload,
+  faMobile,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faGithub, faFacebook);
 
@@ -29,10 +34,55 @@ const About = () => {
           <li>jquery</li>
           <li>c-sharp</li>
           <li>typescript</li>
+          <li>xamarin</li>
         </ul>
         <div className="download-cv">
           Baixar Currículo &nbsp;&nbsp;
           <FontAwesomeIcon icon={faDownload} />
+        </div>
+      </div>
+      <div className="services">
+        <h1>O que eu faço</h1>
+        <div className="services-items">
+          <div className="service-item web">
+            <h2 className="service-title">
+              <FontAwesomeIcon icon={faCode} />
+              &nbsp; Desenvolvimento Web
+            </h2>
+            <br />
+            <p className="service-text">
+              Sou um desenvolvedor web com experiência em .NET. Trabalho com
+              ASP.NET, C#, MVC, Entity Framework e React. Meu foco é criar
+              soluções web eficientes e escaláveis. Busco oportunidades para
+              aplicar minhas habilidades em projetos desafiadores e inovadores.
+            </p>
+          </div>
+          <div className="service-item mobile">
+            <h2 className="service-title">
+              <FontAwesomeIcon icon={faMobile} />
+              &nbsp; Desenvolvimento Mobile
+            </h2>
+            <br />
+            <p className="service-text">
+              Sou um desenvolvedor mobile com experiência em Xamarin. Trabalho
+              com C# para criar aplicativos nativos para Android e iOS. Busco
+              oportunidades para aplicar minha experiência em desenvolvimento
+              mobile em projetos inovadores e desafiadores.
+            </p>
+          </div>
+          <div className="service-item database">
+            <h2 className="service-title">
+              <FontAwesomeIcon icon={faDatabase} />
+              &nbsp; SQL Server
+            </h2>
+            <br />
+            <p className="service-text">
+              Tenho experiência em gerenciamento de Banco de Dados SQL Server.
+              Trabalho com modelagem, consultas e otimização de desempenho.
+              Busco oportunidades para aplicar minha experiência em projetos que
+              exijam um sólido conhecimento em SQL Server.
+            </p>
+          </div>
         </div>
       </div>
     </div>
